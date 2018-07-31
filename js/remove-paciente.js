5a410b0b6f2c88f176d025c3c2fb3f2ff8b1077c
Fernando Silva <fernandosilvadj@hotmail.com>
2018-07-31 20:05:09 -0300

    Removendo pacientes da tabela

---
+++ js/remove-paciente.js
@@ -0,0 +1,14 @@
+let tabela = document.querySelector('.corpo-tabela');
+
+tabela.addEventListener('dblclick', (event) => {
+    let alvo = event.target.parentNode;
+    alvo.classList.add("fade-out");
+    setTimeout(() => {
+        removePaciente(alvo);
+    }, 500);
+})
+
+
+function removePaciente(alvo) {
+    alvo.remove();
+}
